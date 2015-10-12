d149fd24018b24e5490a5a97929344e91ae6dd88
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-12 21:33:30 +0000

    wIP

--- subscribers.py
+++ subscribers.py
@@ -11,10 +11,24 @@ logger = __import__('logging').getLogger(__name__)
 
 from zope import component
 
+from zope.security.interfaces import NoInteraction
+from zope.security.management import getInteraction
+from zope.security.management import queryInteraction
+
 from nti.dataserver_core.interfaces import IRecordableAnnotatable
 
 from nti.externalization.interfaces import IObjectModifiedFromExternalEvent
 
+from .record import Record
+
+def principal():
+    try:
+        return getInteraction().participations[0].principal
+    except (NoInteraction, IndexError, AttributeError):
+        return None
+
 @component.adapter(IRecordableAnnotatable, IObjectModifiedFromExternalEvent)
 def _record_modification(obj, event):
-    pass
+    if queryInteraction() is None:
+        return
+    Record()
