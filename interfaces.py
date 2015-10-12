d19c8f3ca548455e0a6cc2f478404efc21711721
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-12 21:07:37 +0000

    adding record history

--- interfaces.py
+++ interfaces.py
@@ -8,4 +8,45 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
+
+from zope.container.interfaces import IContained
+
+from dolmen.builtins.interfaces import IIterable
+
+from nti.dataserver_core.interfaces import ICreated
+
+from nti.schema.field import Bool
+from nti.schema.field import Dict
+from nti.schema.field import Date
+from nti.schema.field import Text
+from nti.schema.field import Number
+from nti.schema.field import Variant
+from nti.schema.field import TextLine
+from nti.schema.field import DateTime
+from nti.schema.field import Timedelta
+from nti.schema.field import ValidTextLine
+
+class IRecord(IContained, ICreated):
+	creator = ValidTextLine(title="The attribute name", required=True)
+	attributes = Dict(title="The modifed attributes",
+				 	  key_type=ValidTextLine(title="The attribute name"),
+					  value_type=
+					  		Variant((Number(title="Number value"),
+							   		 Bool(title='Boolean value'),
+							   		 Date(title='Date value'),
+							   		 Text(title='Text value'),
+							   		 DateTime(title='Datetime value'),
+							   		 Timedelta(title='Timedelta value'),
+							   		 TextLine(title='String value'),), title="The value"),
+					  min_length=1)
+
+class IRecordHistory(IContained, IIterable):
 	
+	def add(record):
+		pass
+	
+	def remove(record):
+		pass
+	
+	def clear(self):
+		pass
---
+++ record.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+from zope import lifecycleevent
+
+from zope.annotation import factory as an_factory
+
+from zope.container.contained import Contained
+
+from zope.location import locate
+from zope.location.interfaces import ISublocations
+
+from zope.mimetype.interfaces import IContentTypeAware
+
+from ZODB.interfaces import IConnection
+
+from persistent import Persistent
+from persistent.list import PersistentList
+
+from nti.coremetadata.interfaces import IRecordable
+
+from nti.dublincore.datastructures import PersistentCreatedModDateTrackingObject
+
+from nti.externalization.representation import WithRepr
+
+from nti.schema.field import SchemaConfigured
+from nti.schema.fieldproperty import createDirectFieldProperties
+
+from .interfaces import IRecord
+from .interfaces import IRecordHistory
+
+from . import RECORD_HISTORY_KEY
+
+@WithRepr
+@interface.implementer(IRecord, IContentTypeAware)
+class Record(PersistentCreatedModDateTrackingObject,
+			 SchemaConfigured,
+			 Contained):
+
+	createDirectFieldProperties(IRecord)
+
+	def __init__(self, *args, **kwargs):
+		SchemaConfigured.__init__(self, *args, **kwargs)
+		PersistentCreatedModDateTrackingObject.__init__(self)
+
+	def add(self, name, value):
+		if self.attributes is None:
+			self.attributes = dict()
+		self.attributes[name] = value
+
+@component.adapter(IRecordable)
+@interface.implementer(IRecordHistory, ISublocations)
+class RecordHistory(Contained, Persistent):
+
+	def __init__(self):
+		self.reset()
+
+	def reset(self):
+		self._records = PersistentList()
+
+	@property
+	def object(self):
+		return self.__parent__
+
+	def add(self, record):
+		# locate before firing events
+		locate(record, self)
+		# add to connection and fire event
+		IConnection(self).add(record)
+		lifecycleevent.created(record)
+		lifecycleevent.added(record)  # get an iid
+
+		return record
+	append = add
+
+	def remove(self, purchase):
+		lifecycleevent.removed(purchase)  # remove iid
+		return True
+
+	def clear(self):
+		result = 0
+		for p in list(self._records):
+			self.remove(p)
+			result += 1
+		return result
+
+	def records(self):
+		return self._records
+
+	def __iter__(self):
+		return iter(self._records)
+
+	def __len__(self):
+		return len(self._records)
+
+	def sublocations(self):
+		for record in self._records:
+			yield record
+
+_RecordHistoryFactory = an_factory(RecordHistory, RECORD_HISTORY_KEY)
