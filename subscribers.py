9e1d46bbe602ac6c56289c3643c3a244e02d6ee3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-10-23 02:31:40 +0000

    add to connection

--- subscribers.py
+++ subscribers.py
@@ -17,6 +17,8 @@ from zope.security.management import queryInteraction
 
 from ZODB.utils import serial_repr
 
+from ZODB.interfaces import IConnection
+
 from nti.coremetadata.interfaces import IRecordable
 
 from nti.externalization.interfaces import IObjectModifiedFromExternalEvent
@@ -25,6 +27,16 @@ from .record import TransactionRecord
 
 from .interfaces import ITransactionRecordHistory
 
+def get_connection(registry=None):
+	registry = component.getSiteManager()
+	result = IConnection(registry, None)
+	return result
+
+def add_2_connection(obj, connection=None):
+	connection = get_connection() if connection is None else connection
+	if connection is not None and IConnection(obj, None) is None:
+		connection.add(object)
+
 def principal():
 	try:
 		return getInteraction().participations[0].principal
@@ -32,20 +44,25 @@ def principal():
 		return None
 
 def record_trax(recordable, descriptions=(), history=None):
-	history = ITransactionRecordHistory(recordable) if history is None else history
-	
-	username = principal().id
-	
+	connection = get_connection()
+	add_2_connection(recordable, connection)
+
+	if history is None:
+		history = ITransactionRecordHistory(recordable)
+		add_2_connection(history, connection)
+
 	tid = getattr(recordable, '_p_serial', None)
 	tid = unicode(serial_repr(tid)) if tid else None
-	
+
 	attributes = set()
 	for a in descriptions or ():
 		attributes.update(a.attributes or ())
+
+	username = principal().id
 	record = TransactionRecord(principal=username, tid=tid,
 							   attributes=tuple(attributes))
 	history.add(record)
-	
+
 	recordable.locked = True
 	return record
 
@@ -53,7 +70,6 @@ def record_trax(recordable, descriptions=(), history=None):
 def _record_modification(obj, event):
 	if queryInteraction() is None:
 		return
-		
 	history = ITransactionRecordHistory(obj, None)
 	if history is None:
 		return
