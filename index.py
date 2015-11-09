ecaefa1652fe24d34fbe297ebf5a2e12ed76f273
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-11-09 21:38:25 +0000

    Add publish/unpublish events

--- index.py
+++ index.py
@@ -5,7 +5,6 @@
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
-
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
