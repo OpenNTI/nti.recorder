9c1b747bc5c70210f7150641e85d2342f106046c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-08 00:18:48 +0000

    pep8

--- index.py
+++ index.py
@@ -69,15 +69,13 @@ IX_CREATEDTIME = 'createdTime'
 #: Transaction principal
 IX_USERNAME = IX_PRINCIPAL = 'principal'
 
+
 deprecated('SiteIndex', 'No longer used')
-
-
 class SiteIndex(RawSetIndex):
     pass
 
+
 deprecated('TargetIntIDIndex', 'No longer used')
-
-
 class TargetIntIDIndex(IntegerAttributeIndex):
     pass
 
@@ -234,7 +232,9 @@ def install_recorder_catalog(site_manager_container, intids=None):
     catalog = MetadataRecorderCatalog(family=intids.family)
     locate(catalog, site_manager_container, CATALOG_NAME)
     intids.register(catalog)
-    lsm.registerUtility(catalog, provided=IMetadataCatalog, name=CATALOG_NAME)
+    lsm.registerUtility(catalog, 
+                        provided=IMetadataCatalog, 
+                        name=CATALOG_NAME)
 
     catalog = create_recorder_catalog(catalog=catalog, family=intids.family)
     for index in catalog.values():
