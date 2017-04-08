924ca6918e0883e23d66467515b8cc9831a2ad40
csanchez <csanchez@192.168.1.7>
2017-04-07 22:05:43 -0500

    add trx catalog test

--- src/nti/recorder/tests/test_index.py
+++ src/nti/recorder/tests/test_index.py
@@ -17,9 +17,13 @@ import BTrees
 import unittest
 
 from nti.recorder.index import get_recordables
+from nti.recorder.index import get_transactions
 from nti.recorder.index import create_recorder_catalog
+from nti.recorder.index import create_transaction_catalog
 
 from nti.recorder.mixins import RecordableMixin
+
+from nti.recorder.record import TransactionRecord
 
 from nti.recorder.tests import SharedConfiguringTestLayer
 
@@ -34,5 +38,17 @@ class TestAdapters(unittest.TestCase):
         catalog = create_recorder_catalog(family=BTrees.family64)
         catalog.super_index_doc(1, recordable)
         uids = list(get_recordables(catalog=catalog))
+        assert_that(uids, has_length(1))
+        assert_that(1, is_in(uids))
+
+    def test_get_trasanction(self):
+        record = TransactionRecord(principal='ichigo', type='create',
+                                   tid='tx123', 
+                                   attributes=('bankai',),
+                                   external_value={'name':'tensa'})
+        record.lastModified = record.createdTime = 100000
+        catalog = create_transaction_catalog(family=BTrees.family64)
+        catalog.super_index_doc(1, record)
+        uids = list(get_transactions(catalog=catalog))
         assert_that(uids, has_length(1))
         assert_that(1, is_in(uids))
