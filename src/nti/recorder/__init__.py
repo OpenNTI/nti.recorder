7c440605e43929fa9990ff153c033e9b89384289
Carlos Sanchez <papachoco@gmail.com>
2017-05-06 17:04:49 -0500

    don't rexport

--- src/nti/recorder/__init__.py
+++ src/nti/recorder/__init__.py
@@ -4,23 +4,13 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
-
-from zope import component
-
-from nti.recorder.index import CATALOG_NAME
 
 from nti.recorder.interfaces import TRX_RECORD_HISTORY_KEY
 
 from nti.recorder.record import get_transactions
 from nti.recorder.record import has_transactions
 from nti.recorder.record import remove_transaction_history
-
-from nti.zope_catalog.interfaces import IMetadataCatalog
-
-
-def get_recorder_catalog():
-    return component.queryUtility(IMetadataCatalog, name=CATALOG_NAME)
