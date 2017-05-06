7f6e1c7b77d3fbdadcf6c20c0b4f8cc06b53e249
Carlos Sanchez <papachoco@gmail.com>
2017-05-06 17:07:31 -0500

    remove export

--- src/nti/recorder/__init__.py
+++ src/nti/recorder/__init__.py
@@ -9,8 +9,6 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
-from nti.recorder.interfaces import TRX_RECORD_HISTORY_KEY
-
 from nti.recorder.record import get_transactions
 from nti.recorder.record import has_transactions
 from nti.recorder.record import remove_transaction_history
