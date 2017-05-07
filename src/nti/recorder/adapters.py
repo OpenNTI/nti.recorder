029ab1a1dc5be1064244f1871862930c0875c9ff
Carlos Sanchez <papachoco@gmail.com>
2017-05-06 23:57:40 -0500

    fix import

--- src/nti/recorder/adapters.py
+++ src/nti/recorder/adapters.py
@@ -24,7 +24,7 @@ from zope.location.location import locate
 
 from ZODB.interfaces import IConnection
 
-from nti.recorder import TRX_RECORD_HISTORY_KEY
+from nti.recorder.interfaces import TRX_RECORD_HISTORY_KEY
 
 from nti.recorder.interfaces import IRecordable
 from nti.recorder.interfaces import ITransactionRecord
