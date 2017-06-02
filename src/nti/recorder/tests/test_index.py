4f9f024a6eb967c369de7b1dc640205c0e218a9c
Carlos Sanchez <papachoco@gmail.com>
2017-06-02 18:52:26 -0500

    test catalogs

--- src/nti/recorder/tests/test_index.py
+++ src/nti/recorder/tests/test_index.py
@@ -7,6 +7,7 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from hamcrest import is_
 from hamcrest import is_in
 from hamcrest import is_not
 from hamcrest import has_length
@@ -16,10 +17,14 @@ does_not = is_not
 import BTrees
 import unittest
 
+
 from nti.recorder.index import get_recordables
 from nti.recorder.index import get_transactions
 from nti.recorder.index import create_recorder_catalog
 from nti.recorder.index import create_transaction_catalog
+
+from nti.recorder.index import MetadataRecorderCatalog
+from nti.recorder.index import MetadataTransactionCatalog
 
 from nti.recorder.mixins import RecordableMixin
 
@@ -35,7 +40,12 @@ class TestAdapters(unittest.TestCase):
     def test_get_recordables(self):
         recordable = RecordableMixin()
         recordable.lock()
+        # test catalog
         catalog = create_recorder_catalog(family=BTrees.family64)
+        assert_that(isinstance(catalog, MetadataRecorderCatalog), 
+                    is_(True))
+        assert_that(catalog, has_length(3))
+        # text index
         catalog.super_index_doc(1, recordable)
         uids = list(get_recordables(catalog=catalog))
         assert_that(uids, has_length(1))
@@ -48,7 +58,12 @@ class TestAdapters(unittest.TestCase):
                                    attributes=(u'bankai',),
                                    external_value={u'name':u'tensa'})
         record.lastModified = record.createdTime = 100000
+        # test catalog
         catalog = create_transaction_catalog(family=BTrees.family64)
+        assert_that(isinstance(catalog, MetadataTransactionCatalog), 
+                    is_(True))
+        assert_that(catalog, has_length(6))
+        # text index
         catalog.super_index_doc(1, record)
         uids = list(get_transactions(catalog=catalog))
         assert_that(uids, has_length(1))
