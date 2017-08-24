b0367ea8d27d7dbbe1c9b44b23cdd2dc5abc5535
Carlos Sanchez <papachoco@gmail.com>
2017-08-23 20:06:21 -0500

    add test

---
+++ src/nti/recorder/tests/test_interfaces.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+import unittest
+
+from zope.dottedname import resolve as dottedname
+
+
+class TestInterfaces(unittest.TestCase):
+
+    def test_import_interfaces(self):
+        dottedname.resolve('nti.contenttypes.presentation.interfaces')
