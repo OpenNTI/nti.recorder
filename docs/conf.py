8efeb6c365762012b4593346e0246e4e70fd37c0
Carlos Sanchez <papachoco@gmail.com>
2017-09-24 16:00:03 -0500

    remove whitespaces

--- docs/conf.py
+++ docs/conf.py
@@ -108,7 +108,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'ntirecorder doc'
+htmlhelp_basename = 'ntirecorderdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -135,7 +135,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'ntirecorder .tex', u'nti.recorder Documentation',
+    (master_doc, 'ntirecorder.tex', u'nti.recorder Documentation',
      u'NextThought', 'manual'),
 ]
 
@@ -145,7 +145,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'ntirecorder ', u'nti.recorder Documentation',
+    (master_doc, 'ntirecorder', u'nti.recorder Documentation',
      [author], 1)
 ]
 
@@ -156,8 +156,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'ntirecorder ', u'nti.recorder Documentation',
-     author, 'ntirecorder ', 'One line description of project.',
+    (master_doc, 'ntirecorder', u'nti.recorder Documentation',
+     author, 'ntirecorder', 'One line description of project.',
      'Miscellaneous'),
 ]
 
